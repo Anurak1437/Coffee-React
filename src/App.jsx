c6738db5ed5c34d3b68348dd7bb2f4edba337691
Anurak1437 <phrmsxnx995@gmail.com>
2025-12-07 23:54:37 +0700

    Anurak Coffee fix

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Components/Header'
+import Content from './Components/Content'
+
+const App = () => {
+  return (
+    <Header/>
+    <Content/>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <nav className="bg-amber-800 text-white shadow-lg">
+        <div className="w-full mx-auto px-6 py-4 flex justify-between items-center">
+            <div className="flex items-center gap-3">
+                <img src="" alt="" />
+                <span className="text-2xl font-bold">Anurak Coffee</span>
+            </div>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Header
